5c603e8c3a813ffed5aca8e724e3bc94b6e7ab87
AliakseiPrystrom <pristrom1991@gmail.com>
2022-05-07 00:33:04 +0300

    delete comments

--- src/test/java/tests/TestCase1.java
+++ src/test/java/tests/TestCase1.java
@@ -5,11 +5,15 @@ import aquality.selenium.browser.Browser;
 import aquality.selenium.core.utilities.ISettingsFile;
 import aquality.selenium.core.utilities.JsonSettingsFile;
 import forms.FirstCardForm;
+import forms.SecondCard;
+import forms.ThirdCardForm;
 import forms.WelcomePageForm;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import utils.Domains;
 import utils.WorkWithParams;
+import utils.WorkWithUpload;
 
 public class TestCase1 extends BaseTest {
     private final ISettingsFile environment = new JsonSettingsFile("testdata.json");
@@ -39,23 +43,23 @@ public class TestCase1 extends BaseTest {
         firstCard.clearEmailFieldAndType(WorkWithParams.chooseTestParamsForLogin());
         firstCard.clearDomainAndTYpe(environment.getValue("/testDomain").toString());
 
-//        firstCard.otherSelect();
-//        firstCard.otherValueSelect(Domains.COM.toString());
-//        firstCard.useLicenseCheckBox();
-//        firstCard.goNextCard();
-//        browser.waitForPageToLoad();
-//        SecondCard secondCard = new SecondCard();
-//        Assert.assertTrue(secondCard.state().waitForDisplayed(), "Card did not open");
-//        secondCard.resetAllCB();
-//        secondCard.selectCB(3);
+        firstCard.otherSelect();
+        firstCard.otherValueSelect(Domains.COM.toString());
+        firstCard.useLicenseCheckBox();
+        firstCard.goNextCard();
+        browser.waitForPageToLoad();
+        SecondCard secondCard = new SecondCard();
+        Assert.assertTrue(secondCard.state().waitForDisplayed(), "Card did not open");
+        secondCard.resetAllCB();
+        secondCard.selectCB(3);
 
 
-//        secondCard.clickUpload();
-//        WorkWithUpload.uploadFile(environment.getValue("/uploadFileName").toString());
-//        secondCard.goNextCard();
-//        browser.waitForPageToLoad();
-//        ThirdCardForm thirdCard = new ThirdCardForm();
-//        Assert.assertTrue(thirdCard.state().waitForDisplayed(), "Card did not open");
+        secondCard.clickUpload();
+        WorkWithUpload.uploadFile(environment.getValue("/uploadFileName").toString());
+        secondCard.goNextCard();
+        browser.waitForPageToLoad();
+        ThirdCardForm thirdCard = new ThirdCardForm();
+        Assert.assertTrue(thirdCard.state().waitForDisplayed(), "Card did not open");
     }
 
 
